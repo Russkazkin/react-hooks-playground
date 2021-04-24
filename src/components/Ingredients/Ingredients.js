a5988c8a2bb8b0d6c62b6f94e412e27e90825156
Russkazkin <ruslan@skazkin.su>
2021-04-24 06:11:32 +0300

    Create axios config file

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import axios from "axios";
+import axios from '../../axios';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -8,7 +8,7 @@ import IngredientList from "./IngredientList";
 const Ingredients = props => {
   const [ingredients, setIngredients] = useState([]);
   const addIngredient = async ingredient => {
-    const response = (await axios.post('https://react-hooks-playground-d70ef-default-rtdb.firebaseio.com/ingredients.json', ingredient)).data;
+    const response = (await axios.post('ingredients.json', ingredient)).data;
     setIngredients(previousIngredients => [...previousIngredients, {...ingredient, id: response.name}]);
   }
   const removeIngredient = id => {

61b6137c3d19a2f6e6f29fdd5110b2e391f2e61b
Russkazkin <ruslan@skazkin.su>
2021-04-26 19:16:50 +0300

    Add fetching ingredients with useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from '../../axios';
 
 import IngredientForm from './IngredientForm';
@@ -7,6 +7,23 @@ import IngredientList from "./IngredientList";
 
 const Ingredients = props => {
   const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = (await axios.get('ingredients.json')).data;
+      const ingredients = [];
+      for (const key in response) {
+        ingredients.push({
+          id: key,
+          title: response[key].title,
+          amount: response[key].amount,
+        });
+      }
+      setIngredients(ingredients);
+    })();
+  }, []);
+
+
   const addIngredient = async ingredient => {
     const response = (await axios.post('ingredients.json', ingredient)).data;
     setIngredients(previousIngredients => [...previousIngredients, {...ingredient, id: response.name}]);

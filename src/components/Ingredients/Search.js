156905216237ec15a5d5a0497076cf98cf084bf4
Russkazkin <ruslan@skazkin.su>
2021-05-02 11:16:24 +0300

    Set timeout to decrease request number

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -7,30 +7,35 @@ import axios from "../../axios";
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [filter, setFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`;
-    (async () => {
-      const response = (await axios.get('ingredients.json' + query)).data;
-      const ingredients = [];
-      for (const key in response) {
-        if(response.hasOwnProperty(key)) {
-          ingredients.push({
-            id: key,
-            title: response[key].title,
-            amount: response[key].amount,
-          });
-        }
+    setTimeout(() => {
+      if (filter === inputRef.current.value) {
+        const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`;
+        (async () => {
+          const response = (await axios.get('ingredients.json' + query)).data;
+          const ingredients = [];
+          for (const key in response) {
+            if(response.hasOwnProperty(key)) {
+              ingredients.push({
+                id: key,
+                title: response[key].title,
+                amount: response[key].amount,
+              });
+            }
+          }
+          onLoadIngredients(ingredients);
+        })();
       }
-      onLoadIngredients(ingredients);
-    })();
-  }, [filter, onLoadIngredients]);
+    }, 500);
+  }, [filter, onLoadIngredients, inputRef]);
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={filter} onChange={event => {setFilter(event.target.value)}} />
+          <input ref={inputRef} type="text" value={filter} onChange={event => {setFilter(event.target.value)}} />
         </div>
       </Card>
     </section>
